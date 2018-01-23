feacab29c88d87ab34e311aaa9d35e4d965a3a77
Yves Schelpe <yves.schelpe@gmail.com>
2018-01-23 01:24:27 +0100

    Expected number of GSAK lines doesn't match - build will fail

---
+++ Source/Backend/Kf.ConverToGSAK.Api.Tests/Input/BarnyGeeft/BarneyGeeftIntegrationTests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Kf.ConverToGSAK.Api.Input.BarnyGeeft;
+using Xunit;
+
+namespace Kf.ConverToGSAK.Api.Tests.Input.BarnyGeeft
+{
+    public sealed class BarneyGeeftIntegrationTests
+    {
+        [Fact]
+        public async void Can_convert_to_gsak_formatted_file() {
+            var expected = ReadFromResource("Kf.ConverToGSAK.Api.Tests.Input.BarnyGeeft.GC5-GSAK-formatted.txt");
+            var expectedNumberOfBarneyGeeftLines = 47787;
+            var expectedNumberOfGsakLines = 20193;
+            var input = ReadFromResource("Kf.ConverToGSAK.Api.Tests.Input.BarnyGeeft.GC5-original.txt");
+
+            var barneyGeeftFile = new BarnyGeeftFile(
+                new BarneyGeeftFileToGsakFileConverter(new BarneyGeeftFileLineToGsakFileLineConverter()), 
+                input
+            );
+            Assert.Equal(expectedNumberOfBarneyGeeftLines, barneyGeeftFile.Lines.Count());
+
+            var gsakFile = await barneyGeeftFile.Convert();
+            Assert.Equal(expectedNumberOfGsakLines, gsakFile.Lines.Count());
+        }
+
+        private string ReadFromResource(string resourceName) {
+            using (var stream = Assembly
+                .GetAssembly(typeof(BarneyGeeftIntegrationTests))
+                .GetManifestResourceStream(resourceName)) {
+                using (var streamReader = new StreamReader(stream, Encoding.UTF8)) {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
--- Source/Backend/Kf.ConverToGSAK.Api/Input/BarnyGeeft/BarnyGeeftFile.cs
+++ Source/Backend/Kf.ConverToGSAK.Api/Input/BarnyGeeft/BarnyGeeftFile.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Kf.ConverToGSAK.Api.Gsak;
 
@@ -6,10 +8,16 @@ namespace Kf.ConverToGSAK.Api.Input.BarnyGeeft
 {
     public sealed class BarnyGeeftFile
     {
-        private readonly IConvertToGsakFile<BarnyGeeftFile> _toGsakFileConverter;
+        private readonly IConvertToGsakFile<BarnyGeeftFile> _toGsakFileConverter;        
 
-        public BarnyGeeftFile(IConvertToGsakFile<BarnyGeeftFile> toGsakFileConverter)
-            => _toGsakFileConverter = toGsakFileConverter;        
+        public BarnyGeeftFile(IConvertToGsakFile<BarnyGeeftFile> toGsakFileConverter, string input = null) {
+            _toGsakFileConverter = toGsakFileConverter;
+
+            if (input != null) {
+                Lines = input.Split(new string[] {"\r\n", "\n"}, StringSplitOptions.None)
+                    .Select(l => new BarnyGeeftFileLine {RawContent = l});
+            }
+        }
 
         public IEnumerable<BarnyGeeftFileLine> Lines { get; set; }
 
